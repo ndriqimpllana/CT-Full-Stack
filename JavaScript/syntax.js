22f2f601ff2167c56fd7bb052a7cac646d0b7993
Ndricim Pllana <ndriqimpllana@gmail.com>
2025-05-16 16:39:50 -0400

    variables

--- JavaScript/syntax.js
+++ JavaScript/syntax.js
@@ -1,1 +1,20 @@
-alert ('Hello, World!')
+// Declare variables
+let income = 2500;
+let sideBusiness = 500;
+let expenses = 1200;
+
+// Calculate total income nad expenses
+
+let totalIncome = income + sideBusiness;
+let totalExpenses = expenses;
+
+// Calculate net balance
+let netBalance = totalIncome - totalExpenses;
+
+// Output the results to hte console
+
+console.log("Total Income:", totalIncome);
+console.log("Total Expenses:", totalExpenses);
+console.log("Net Balance:", netBalance);
+
+
