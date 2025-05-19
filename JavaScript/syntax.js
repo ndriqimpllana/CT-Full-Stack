23431fff3a4feb4f7ad978605d8dede47842c9e5
Ndricim Pllana <ndriqimpllana@gmail.com>
2025-05-19 14:35:43 -0400

    j

--- JavaScript/syntax.js
+++ JavaScript/syntax.js
@@ -1,22 +1,11 @@
-console.log(typeof 42); // Output: "Number"
-console.log(typeof "Hello World!"); // Output: "String"
-console.log(typeof true); // Output: "boolean"
-console.log(typeof {name: "John", age: 30}); // Output: "Object"
-console.log(typeof [1, 2, 3]); // Output: "object"
-console.log(typeof undefined); // Output: "Undefined"
 
 
-console.log(5 + "5"); // ==> '55'
-console.log("10" - 5); // ==> 5
-console.log("true" == true); // ==> false
-console.log(+"100"); // ==> 100
+
+let age = 18;
 
 
-console.log(0 == false);
-console.log("" == false);
-console.log(0 === false);
-console.log("" === false);
-
-
-console.log("4" == 4); // ==> If the value is equal
-console.log("4" === 4); // ==> If the data type and the value are equal
+if (age >= 18) {
+    console.log('You are and adult.');
+}else {
+    console.log('You are not adult.');
+    }
