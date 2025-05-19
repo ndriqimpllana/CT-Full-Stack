009e5341a93e2679efb0ef6835e6acd01e312ba1
Ndricim Pllana <ndriqimpllana@gmail.com>
2025-05-19 15:33:11 -0400

    if statements

--- JavaScript/syntax.js
+++ JavaScript/syntax.js
@@ -9,3 +9,15 @@ if (age >= 18) {
 }else {
     console.log('You are not adult.');
     }
+
+
+let score = 85;
+
+if (score >= 90) {
+    console.log('Grade: A');
+} else if (score >=75) {
+    console.log('Grade: B');
+} else {
+    console.log("Grade C or lower");
+}
+
