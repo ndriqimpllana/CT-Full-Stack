323b9e89c495f0e85ffc8e11549a25c82d14d4c0
Ndricim Pllana <ndriqimpllana@gmail.com>
2025-05-20 23:01:05 -0400

    if statements

--- JavaScript/syntax.js
+++ JavaScript/syntax.js
@@ -1,12 +1,22 @@
-let age = 18;
-let hasID = true;
+let age = 30;
+let hasVIPPass = false;
 
-if (age >= 18) {
-    if(hasID) {
-        console.log("You can enter");
-    } else {
-        console.log("You need ID.");
-    } else {
-        console.log("You are too young to enter.");
-    }
+if (age < 5) {
+    console.log("Free Entry!");
+} else if (age >= 5 && age <= 12) {
+    console.log("Child ticket: $10");
+} else if (age >= 13 && age <= 64) {
+    console.log("Adult Ticket: $20");
+} else {
+    console.log("Senior Ticket: $15");
 }
+
+if (hasVIPPass) {
+    console.log("VIP Access Granted");
+}
+
+age = 12 
+console.log(age);
+
+hasVIPPass = true;
+console.log(hasVIPPass);
